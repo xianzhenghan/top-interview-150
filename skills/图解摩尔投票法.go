16ad1a75ed9e4bf2bd1a126c073113b933e02f44
xianzheng.han <xianzheng.han@xinfei.cn>
2024-09-12 16:10:16 +0800

    add CODE

---
+++ skills/图解摩尔投票法.go
@@ -0,0 +1,24 @@
+package main
+
+import "fmt"
+
+/**
+算法描述
+摩尔投票法（Boyer–Moore majority vote algorithm），也被称作「多数投票法」，算法解决的问题是：如何在任意多的候选人中（选票无序），选出获得票数最多的那个。
+
+算法可以分为两个阶段：
+
+对抗阶段：分属两个候选人的票数进行两两对抗抵消
+计数阶段：计算对抗结果中最后留下的候选人票数是否有效
+这样说比较抽象，我们直接来看一道题：LeetCode 169. 多数元素
+
+作者：江不知
+链接：https://leetcode.cn/problems/majority-element/solutions/154941/tu-jie-mo-er-tou-piao-fa-python-go-by-jalan/
+来源：力扣（LeetCode）
+著作权归作者所有。商业转载请联系作者获得授权，非商业转载请注明出处。
+*/
+//https://leetcode.cn/problems/majority-element/solutions/154941/tu-jie-mo-er-tou-piao-fa-python-go-by-jalan/?envType=study-plan-v2&envId=top-interview-150
+
+func main() {
+	fmt.Println("sss")
+}
